bb51b1c820ae9331591e3c7b4ccc6e3f8a1a5347
diogosmac <diogo.smac@gmail.com>
2019-04-05 16:07:54 +0100

    Implemented current constraints

--- entrega 2/criar.sql
+++ entrega 2/criar.sql
@@ -1,148 +1,223 @@
 --
--- File generated with SQLiteStudio v3.2.1 on qua abr 3 17:51:25 2019
+-- File generated with SQLiteStudio v3.2.1 on sex abr 5 15:57:30 2019
 --
--- Text encoding used: UTF-8
+-- Text encoding used: System
 --
-PRAGMA foreign_keys = on;
+PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
 -- Table: AlugFilme
 DROP TABLE IF EXISTS AlugFilme;
+
 CREATE TABLE AlugFilme (
-    idAluguer INTEGER,
-    idFilme INTEGER,
-    PRIMARY KEY (idAluguer, idFilme)
+    idAluguer INTEGER CONSTRAINT fk_alugfilme_idaluguer REFERENCES Aluguer (idAluguer),
+    idFilme   INTEGER CONSTRAINT fk_alugfilme_idfilme REFERENCES Filme,
+    PRIMARY KEY (
+        idAluguer,
+        idFilme
+    )
 );
+
 
 -- Table: Aluguer
 DROP TABLE IF EXISTS Aluguer;
+
 CREATE TABLE Aluguer (
-    idAluguer INTEGER PRIMARY KEY,
-    dataAluguer DATETIME,
-    preçoAluguer REAL,
-    idLoja INTEGER,
-    idCliente INTEGER
+    idAluguer    INTEGER PRIMARY KEY,
+    dataAluguer  DATE    CONSTRAINT nn_alugues_dataaluguer NOT NULL,
+    precoAluguer REAL,
+    idLoja       INTEGER CONSTRAINT fk_aluguer_idloja REFERENCES Loja (idLoja),
+    idCliente    INTEGER CONSTRAINT fk_aluguer_idcliente REFERENCES Cliente (idPessoa) 
 );
+
 
 -- Table: AluguerAtual
 DROP TABLE IF EXISTS AluguerAtual;
+
 CREATE TABLE AluguerAtual (
-    idAluguer INTEGER PRIMARY KEY,
-    dataLimite DATE
+    idAluguer  INTEGER PRIMARY KEY
+                       CONSTRAINT fk_alugueratual_idaluguer REFERENCES Aluguer (idAluguer),
+    dataLimite DATE    CONSTRAINT nn_alugueratual_datalimite NOT NULL
 );
+
 
 -- Table: AluguerPassado
 DROP TABLE IF EXISTS AluguerPassado;
+
 CREATE TABLE AluguerPassado (
-    idAluguer INTEGER PRIMARY KEY,
-    dataLimite DATE,
-    dataEntrega DATE
+    idAluguer   INTEGER PRIMARY KEY
+                        CONSTRAINT fk_aluguerpassado_idaluguer REFERENCES Aluguer (idAluguer),
+    dataLimite  DATE    CONSTRAINT nn_aluguerpassado_datalimite NOT NULL,
+    dataEntrega DATE    CONSTRAINT nn_aluguerpassado_dataentrega NOT NULL,
+    CONSTRAINT check_aluguerpassado_datas CHECK (dataLimite >= dataEntrega) 
 );
+
 
 -- Table: Cliente
 DROP TABLE IF EXISTS Cliente;
+
 CREATE TABLE Cliente (
-    idPessoa INTEGER PRIMARY KEY,
-    dataAdesão DATE
+    idPessoa   INTEGER PRIMARY KEY
+                       CONSTRAINT fk_cliente_idpessoa REFERENCES Pessoa (idPessoa),
+    dataAdesao DATE
 );
+
 
 -- Table: ClienteGold
 DROP TABLE IF EXISTS ClienteGold;
+
 CREATE TABLE ClienteGold (
-    idCliente INTEGER PRIMARY KEY,
-    dataSubscrição DATE
+    idCliente      INTEGER PRIMARY KEY
+                           CONSTRAINT fk_clientegold_idpessoa REFERENCES Cliente (idPessoa),
+    dataSubscricao DATE
 );
+
 
 -- Table: ClienteSilver
 DROP TABLE IF EXISTS ClienteSilver;
+
 CREATE TABLE ClienteSilver (
-    idCliente INTEGER PRIMARY KEY,
-    dataSubscrição DATE
+    idCliente      INTEGER PRIMARY KEY
+                           CONSTRAINT fk_clientesilver_idcliente REFERENCES Cliente (idPessoa),
+    dataSubscricao DATE
 );
 
--- Table: Coleção
-DROP TABLE IF EXISTS Coleção;
-CREATE TABLE Coleção (
-    idColeção INTEGER PRIMARY KEY,
-    nome TEXT,
-    tempoDuração TEXT,
-    numFilmes INTEGER,
-    preço REAL
+
+-- Table: Colecao
+DROP TABLE IF EXISTS Colecao;
+
+CREATE TABLE Colecao (
+    idColecao    INTEGER PRIMARY KEY,
+    nome         TEXT    CONSTRAINT nn_colecao_nome NOT NULL,
+    tempoDuracao TEXT,
+    numFilmes    INTEGER,
+    preco        REAL
 );
+
 
 -- Table: Filme
 DROP TABLE IF EXISTS Filme;
+
 CREATE TABLE Filme (
-    idFilme INTEGER PRIMARY KEY,
-    nome TEXT,
-    género CHAR (1),
-    duração TEXT,
-    idioma TEXT,
-    anoLançamento INTEGER,
-    preço REAL,
-    idColeção INTEGER
+    idFilme       INTEGER  PRIMARY KEY,
+    nome          TEXT     CONSTRAINT nn_filme_nome NOT NULL,
+    genero        CHAR (1) CONSTRAINT check_filme_genero CHECK (genero = 'ACAO' OR 
+                                                                genero = 'COMEDIA' OR 
+                                                                genero = 'ROMANCE' OR 
+                                                                genero = 'TERROR' OR 
+                                                                genero = 'DOCUMENTARIO'),
+    duracao       TEXT,
+    idioma        TEXT     CONSTRAINT check_filme_idioma CHECK (idioma = 'PT' OR 
+                                                                idioma = 'FR' OR 
+                                                                idioma = 'EN' OR 
+                                                                idioma = 'ES' OR 
+                                                                idioma = 'JP'),
+    anoLancamento INTEGER,
+    preco         REAL     CONSTRAINT nn_filme_preco NOT NULL
+                           CONSTRAINT check_filme_preco CHECK (preco > 0),
+    idColecao     INTEGER  CONSTRAINT fk_filme_idcolecao REFERENCES Cole��o (idCole��o) 
 );
 
--- Table: Funcionário
-DROP TABLE IF EXISTS Funcionário;
-CREATE TABLE Funcionário (
-    idPessoa INTEGER PRIMARY KEY,
-    salário INTEGER,
-    idLoja INTEGER
+
+-- Table: Funcionario
+DROP TABLE IF EXISTS Funcionario;
+
+CREATE TABLE Funcionario (
+    idPessoa INTEGER PRIMARY KEY
+                     CONSTRAINT fk_funcionario_idpessoa REFERENCES Pessoa (idPessoa),
+    salario  INTEGER,
+    idLoja   INTEGER CONSTRAINT fk_funcionario_idloja REFERENCES Loja (idLoja) 
 );
 
--- Table: Horário
-DROP TABLE IF EXISTS Horário;
-CREATE TABLE Horário (
-    idHorário INTEGER PRIMARY KEY,
-    diaSemana TEXT,
-    horaInicio TEXT,
-    horaFim TEXT
+
+-- Table: Horario
+DROP TABLE IF EXISTS Horario;
+
+CREATE TABLE Horario (
+    idHorario  INTEGER PRIMARY KEY,
+    diaSemana  TEXT    CONSTRAINT check_horario_diasemana CHECK ( (diaSemana = "SEGUNDA-FEIRA" OR 
+                                                                   diaSemana = "TERCA-FEIRA" OR 
+                                                                   diaSemana = "QUARTA-FEIRA" OR 
+                                                                   diaSemana = "QUINTA-FEIRA" OR 
+                                                                   diaSemana = "SEXTA-FEIRA" OR 
+                                                                   diaSemana = "SABADO" OR 
+                                                                   diaSemana = "DOMINGO") ),
+    horaInicio TIME,
+    horaFim    TIME,
+    CONSTRAINT unique_horario_dia_horas UNIQUE (
+        diaSemana,
+        horaInicio,
+        horaFim
+    ),
+    CONSTRAINT check_horario_horas CHECK (horaFim > horaInicio) 
 );
 
--- Table: HorárioFunc
-DROP TABLE IF EXISTS HorárioFunc;
-CREATE TABLE HorárioFunc (
-    idFuncionário INTEGER,
-    idHorário INTEGER,
-    PRIMARY KEY (idFuncionário, idHorário)
+
+-- Table: HorarioFunc
+DROP TABLE IF EXISTS HorarioFunc;
+
+CREATE TABLE HorarioFunc (
+    idFuncionario INTEGER CONSTRAINT fk_horariofunc_idfuncionario REFERENCES Funcionario (idPessoa),
+    idHorario     INTEGER CONSTRAINT fk_horariofunc_idhorario REFERENCES Horario (idHorario),
+    PRIMARY KEY (
+        idFuncionario,
+        idHorario
+    )
 );
 
--- Table: HorárioLoja
-DROP TABLE IF EXISTS HorárioLoja;
-CREATE TABLE HorárioLoja (
-    idLoja INTEGER,
-    idHorário INTEGER,
-    PRIMARY KEY (idLoja, idHorário)
+
+-- Table: HorarioLoja
+DROP TABLE IF EXISTS HorarioLoja;
+
+CREATE TABLE HorarioLoja (
+    idLoja    INTEGER CONSTRAINT fk_horarioloja_idloja REFERENCES Loja (idLoja),
+    idHorario INTEGER CONSTRAINT fk_horarioloja_idhorario REFERENCES Horario (idHorario),
+    PRIMARY KEY (
+        idLoja,
+        idHorario
+    )
 );
+
 
 -- Table: Loja
 DROP TABLE IF EXISTS Loja;
+
 CREATE TABLE Loja (
-    idLoja INTEGER PRIMARY KEY,
-    local TEXT UNIQUE,
-    telefone INTEGER UNIQUE,
-    idGerente INTEGER UNIQUE
+    idLoja    INTEGER PRIMARY KEY,
+    local     TEXT    CONSTRAINT unique_loja_local UNIQUE
+                      CONSTRAINT nn_loja_local NOT NULL,
+    telefone  INTEGER CONSTRAINT unique_loja_telefone UNIQUE,
+    idGerente INTEGER CONSTRAINT unique_loja_idgerente UNIQUE
+                      CONSTRAINT fk_loja_idgerente REFERENCES Funcionario (idPessoa) 
+                      CONSTRAINT nn_loja_idgerente NOT NULL
 );
+
 
 -- Table: Pessoa
 DROP TABLE IF EXISTS Pessoa;
+
 CREATE TABLE Pessoa (
-    idPessoa INTEGER PRIMARY KEY,
-    nome TEXT,
-    dataNascimento DATE,
-    morada TEXT,
-    telefone INTEGER
+    idPessoa       INTEGER PRIMARY KEY,
+    nome           TEXT    CONSTRAINT nn_pessoa_nome NOT NULL,
+    dataNascimento DATE    CONSTRAINT nn_pessoa_datanascimento NOT NULL,
+    morada         TEXT,
+    telefone       INTEGER CONSTRAINT nn_pessoa_telefone NOT NULL
 );
+
 
 -- Table: Stock
 DROP TABLE IF EXISTS Stock;
+
 CREATE TABLE Stock (
-    idLoja INTEGER,
-    idFilme INTEGER,
-    numExemplares INTEGER,
-    PRIMARY KEY (idLoja, idFilme)
+    idLoja        INTEGER CONSTRAINT fk_stock_idloja REFERENCES Loja (idLoja),
+    idFilme       INTEGER CONSTRAINT fk_stock_idfilme REFERENCES Filme,
+    numExemplares INTEGER CONSTRAINT check_stock_numexemplares CHECK (numExemplares > 0),
+    PRIMARY KEY (
+        idLoja,
+        idFilme
+    )
 );
+
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
