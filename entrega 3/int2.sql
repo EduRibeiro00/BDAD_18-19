0ef608d0534ad03f56a3200e8ebc6f75283d781e
EduRibeiro00 <eribeiro306@gmail.com>
2019-05-14 15:18:24 +0100

    Query number 2 done. Queries: 5/10

--- entrega 3/int2.sql
+++ entrega 3/int2.sql
@@ -6,10 +6,12 @@ create view auxViewHorarioFunc as
     select idFuncionario as idPessoa, idHorario
     from HorarioFunc;
 
--- FALTA TRATAR DO FACTO QUE DA RESET QUANDO E MAIS DE 24H
+-- NOTA: nao foi usada a funcao strftime() uma vez que esta impoe um limite de 24 no campo das horas,
+--       e ha funcionarios que trabalham mais de 24 horas por semana. 
 
-select idPessoa, nome, salario, time(sum(abs(strftime('%s', horaFim) - strftime('%s', horaInicio))), 'unixepoch') as numHoras
+select idPessoa, nome, salario, (sum(abs(strftime('%s', horaFim) - strftime('%s', horaInicio)))) / 3600 as numHoras
 from   Funcionario natural join auxViewHorarioFunc natural join Horario natural join Pessoa
-group by idPessoa;
+group by idPessoa
+order by numHoras DESC;
 
 drop view auxViewHorarioFunc;
