c7be590ede3bd949f4e272540f245cac34a43d8b
EduRibeiro00 <eribeiro306@gmail.com>
2019-05-15 19:48:13 +0100

    Mais uma bem linda

---
+++ entrega 3/int7.sql
@@ -0,0 +1,15 @@
+.mode columns
+.headers on
+.nullvalue NULL
+
+
+select (select count(*)
+        from (select idPessoa from Cliente
+              EXCEPT
+              select idCliente from ClienteGold
+              EXCEPT
+              select idCliente from ClienteSilver)) as numClientesNormais,
+
+        (select count(*) from ClienteGold) as numClientesGold,
+
+        (select count(*) from ClienteSilver) as numClientesSilver;
