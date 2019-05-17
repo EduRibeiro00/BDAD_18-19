f4c9281cf62557d14f46d80afc064c09e81bf51e
diogosmac <diogo.smac@gmail.com>
2019-05-17 15:50:14 +0100

    Queries done. Only triggers left

--- entrega 3/int5.sql
+++ entrega 3/int5.sql
@@ -2,44 +2,27 @@
 .headers on
 .nullvalue NULL
 
-/*
-create view lucroFilme as
+-- contem o lucro de cada filme
+create view auxFilmeLucro as
     select idFilme, genero, nome, (preco * count(*)) as lucro
     from AlugFilme natural join Filme
     group by idFilme;
 
-create view filmeMaisLucrativo as
-    select genero, nome, idFilme
-    from lucroFilme T1
-    where lucro = (select max(lucro) from lucroFilme T2 where T1.genero = T2.genero group by genero);
+-- contem os valores de lucro total e lucro medio para cada genero
+create view auxStatsGenero as
+    select genero, sum(lucro) as sumLucro, avg(lucro) as avgLucro
+    from auxFilmeLucro
+    group by genero;
 
 
+select genero, sumLucro, avgLucro, idFilme, nome
+from auxFilmeLucro natural join auxStatsGenero as T1
+where lucro = (
+    select max(T2.lucro) 
+    from auxFilmeLucro as T2
+    where T1.genero = T2.genero
+)
+group by idFilme;
 
-
-select genero, sum(preco) as sumPreco, avg(preco) as avgPreco, filmeMaisLucrativo.idFilme, filmeMaisLucrativo.nome
-from filmeMaisLucrativo join Filme using(genero)
-group by genero;
-
-
-drop view filmeMaisLucrativo;
-drop view lucroFilme;
-*/
-
-create view lucroFilme as
-    select idFilme, genero, nome, (preco * count(*)) as lucro
-    from AlugFilme natural join Filme
-    group by idFilme;
-
-create view filmeMaisLucrativo as
-    select genero, nome, idFilme
-    from lucroFilme T1
-    where lucro = (select max(lucro) from lucroFilme T2 where T1.genero = T2.genero group by genero);
-
-
-select genero, sum(preco) as sumPreco, avg(preco) as avgPreco, filmeMaisLucrativo.idFilme, filmeMaisLucrativo.nome
-from filmeMaisLucrativo join Filme using(genero)
-group by genero;
-
-
-drop view filmeMaisLucrativo;
-drop view lucroFilme;
+drop view auxStatsGenero;
+drop view auxFilmeLucro;
